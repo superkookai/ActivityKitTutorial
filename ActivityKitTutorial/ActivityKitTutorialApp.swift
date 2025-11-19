97868e32e44d5169feb5d5bac138188675e071c8
superkookai <weerawut@live.com>
2025-11-19 10:49:21 +0700

    Initial Commit

---
+++ ActivityKitTutorial/ActivityKitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityKitTutorialApp.swift
+//  ActivityKitTutorial
+//
+//  Created by Weerawut Chaiyasomboon on 19/11/2568.
+//
+
+import SwiftUI
+
+@main
+struct ActivityKitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
